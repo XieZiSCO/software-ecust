366dcfb91580cec57cf6e59bd60cf47f9c710aef
22070062 <1521960157@qq.com>
2025-06-11 17:31:53 +0800

    增加了测试模式和真实模式的开关，加快了测试时间

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ DEEPSEEK_API_KEY = "sk-83027fc164114fa6bfaa98254f887aec"
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:280204353jyc@localhost/software?charset=utf8mb4"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-TEST_MODE = False  # 改成 False 即可切换为真实模式
+TEST_MODE = False  # 改成 False 即可切换为真实模式 改成 Ture 会输出预设内容而不调用大模型
